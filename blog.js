e9d47d344d4a2dc891a731a5d647675e6e0e9e20
Gabriela Perez <gaby@perez.co.nz>
2023-04-17 00:51:19 +1200

    Added images

---
+++ blog.js
@@ -0,0 +1,15 @@
+// let friends = ['Tony', 'Rosa', 'Fred']
+// const elephantPoluplation = [23, 5, 6, 78]
+
+// // index
+// // [0] Tony
+// // [1] Rosa
+// // [3] Fred
+
+// const coat ={
+//   brand: true,
+//   fabric: "Wool",
+//   toggles: 12
+// }
+
+// coat.fabric = "Silk"
